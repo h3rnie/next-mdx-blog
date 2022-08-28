cb844d2a8d796523bac6a1efebda78ebba9fae4f
Hernie <80145490+h3rnie@users.noreply.github.com>
2022-08-29 01:12:14 +0800

    29/8/22 update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
     // Uncomment to enable Edge Runtime for server rendering
     // https://nextjs.org/docs/api-reference/edge-runtime
@@ -5,6 +11,7 @@ const nextConfig = {
       runtime: "experimental-edge",
   }, */
     reactStrictMode: true,
+    swcMinify: true,
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
@@ -12,4 +19,4 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
